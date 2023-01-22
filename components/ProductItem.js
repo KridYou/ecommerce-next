682daab15b7e9835da985b84eb657ad18242aa61
KridYou <krid123435@hotmail.com>
2023-01-22 21:46:07 +0700

    first commit

---
+++ components/ProductItem.js
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+
+export default function ProductItem({ product, addToCartHandler }) {
+  return (
+    <div className="card flex flex-col justify-between">
+      <Link href={`/product/${product.slug}`}>
+        <Image
+          src={product.image}
+          alt={product.name}
+          width={500}
+          height={250}
+          className="rounded shadow"
+        />
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ utils/data.js
@@ -0,0 +1,70 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'John',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jane',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      name: 'Item1',
+      slug: 'item1',
+      category: 'cat1',
+      image: '/../public/images/doll1.jpg',
+      price: 50,
+      brand: 'brand1',
+      rating: 5.5,
+      numReviews: 8,
+      countInStock: 0,
+      description: 'new release1',
+    },
+    {
+      name: 'Item2',
+      slug: 'item2',
+      category: 'cat2',
+      image: '/../public/images/doll2.jpg',
+      price: 50,
+      brand: 'brand2',
+      rating: 5.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'new release2',
+    },
+    {
+      name: 'Item3',
+      slug: 'item3',
+      category: 'cat3',
+      image: '/../public/images/doll3.jpg',
+      price: 50,
+      brand: 'brand3',
+      rating: 5.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'new release3',
+    },
+    {
+      name: 'Item4',
+      slug: 'item4',
+      category: 'cat4',
+      image: '/../public/images/doll4.jpg',
+      price: 50,
+      brand: 'brand4',
+      rating: 5.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'new release4',
+    },
+  ],
+};
+
+export default data;
